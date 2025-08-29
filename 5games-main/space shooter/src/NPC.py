e89964b423a8c9c577ebbb8447eec94e91219714
ivan <mr.dolcegabana@mail.ru>
2025-08-29 20:37:39 +0300

    init project

---
+++ 5games-main/space shooter/src/NPC.py
@@ -0,0 +1,35 @@
+import pygame
+
+from AbsEntity import AbsEntity
+
+
+class Entity(AbsEntity):
+    def __init__(
+        self,
+        entity_name: str,
+        entity_id: int,
+        groups: tuple[pygame.sprite.AbstractGroup],
+        img_path: str,
+        start_pos_x: float,
+        start_pos_y: float,
+        collidable: bool,
+        hp: float | None = None,
+        max_speed: float | None = None,
+        mass: float | None = None,
+        volume: float | None = None
+    ) -> None:
+
+        super().__init__(
+            entity_name,
+            entity_id,
+            groups,
+            img_path,
+            start_pos_x,
+            start_pos_y,
+            collidable
+        )
+
+        self.hp = hp
+        self.max_speed = max_speed
+        self.mass = mass
+        self.volume = volume
